b58c164810758be1bcc1c1f99ba148245030f43a
Alexander Guryanov <caiiiycuk@gmail.com>
2023-03-12 13:00:55 +0100

    add win3.11

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         win1x: resolve(__dirname, 'win1x.html'),
         win2x: resolve(__dirname, 'win2x.html'),
         win30: resolve(__dirname, 'win30.html'),
+        win311: resolve(__dirname, 'win311.html'),
       },
     },
   },
